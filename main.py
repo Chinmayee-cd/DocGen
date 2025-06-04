4a46230d74a08ef18e6d14f3b1f9ce7e4bffab80
Chinmayee-cd <147172178+Chinmayee-cd@users.noreply.github.com>
2025-06-04 17:20:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from git import Repo
+from git_repo import clone_repo,update_repo,monitor_repo
+from code_parser import extract_metadata
+repo_url = 'https://github.com/Chinmayee-cd/Gestify'
+local_dir = r'C:\Users\chinm\DocGen\dir'
+
+clone_repo(repo_url,local_dir)
+update_repo(repo_url,local_dir)
+monitor_repo(local_dir,interval=300)
+
+metadata=extract_metadata(local_dir)
+print(metadata)
